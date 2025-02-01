8417831c9c4606dbea1f420c0716750ca723a482
spaceybread <spaceybread@gmail.com>
2025-02-01 16:09:04 -0500

    more work

---
+++ SE/rev_key.py
@@ -0,0 +1,32 @@
+import json
+
+def save_to_json(data, filename="rev_keywords.json"):
+    try:
+        with open(filename, "r") as file:
+            existing_data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = {}
+
+    # Add new data to the existing JSON data
+    existing_data.update(data)
+
+    # Write the updated data back to the file
+    with open(filename, "w") as file:
+        json.dump(existing_data, file, indent=4)
+
+
+with open("keywords.json", "r") as f:
+    data = json.load(f)
+
+ma = {}
+for entry in data:
+    ke = data[entry]
+    for ki in ke:
+        k = ki.lower()
+        if ma.get(k) is not None and entry not in ma.get(k):
+            ma[k].append(entry)
+        else:
+            ma[k] = [entry]
+
+save_to_json(ma)
+
--- SE/search.py
+++ SE/search.py
@@ -1,3 +1,24 @@
-quer = "what were the commonwealth games?"
+import re
+import json
+
+with open("rev_keywords.json", "r") as f:
+    data = json.load(f)
+    
+q = "cow is algorithm and race?"
+words = [t.lower() for t in (re.sub('[^a-zA-Z]+', ' ', q)).split()]
+
+poss = {}
+for w in words:
+    if w not in data: continue
+    print(w)
+    li = data[w]
+
+    for l in li:
+        if l in poss:
+            poss[l] += 1
+        else:
+            poss[l] = 1
+
+print(poss)
 
 
